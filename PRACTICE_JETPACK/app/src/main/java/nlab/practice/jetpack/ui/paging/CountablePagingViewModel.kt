359e56e6adec2f02b0ddde6a9a01a8309b66a3a5
Doohyun Nam <skaengus2012@naver.com>
2019-01-24 00:40:59 +0900

    [ISSUE-PAGING] TITLE 수정

--- PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/paging/CountablePagingViewModel.kt
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/paging/CountablePagingViewModel.kt
@@ -1,5 +1,6 @@
 package nlab.practice.jetpack.ui.paging
 
+import androidx.databinding.ObservableField
 import androidx.paging.DataSource
 import androidx.paging.PagedList
 import androidx.paging.RxPagedListBuilder
@@ -8,6 +9,7 @@ import io.reactivex.rxkotlin.addTo
 import nlab.practice.jetpack.R
 import nlab.practice.jetpack.repository.PagingItemRepository
 import nlab.practice.jetpack.util.ResourceProvider
+import nlab.practice.jetpack.util.SchedulerFactory
 import nlab.practice.jetpack.util.component.ActivityCommonUsecase
 import nlab.practice.jetpack.util.recyclerview.RecyclerViewConfig
 import nlab.practice.jetpack.util.recyclerview.paging.BindingPagedListAdapter
@@ -20,17 +22,20 @@ import javax.inject.Inject
 class CountablePagingViewModel @Inject constructor(
         private val _disposables: CompositeDisposable,
         private val _activityCommonUsecase: ActivityCommonUsecase,
+        private val _pagingItemRepository: PagingItemRepository,
+        private val _schedulerFactory: SchedulerFactory,
         resourceProvider: ResourceProvider,
-        pagingItemRepository: PagingItemRepository,
         pagingManagerFactory: CountablePositionalPagingManager.Factory) {
 
     val listAdapter: BindingPagedListAdapter<PagingItemViewModel> = BindingPagedListAdapter.create()
     val recyclerViewConfig = RecyclerViewConfig()
-    val subTitle = resourceProvider.getString(R.string.paging_countable_sub_title)
+    val subTitle = ObservableField<String>()
+    private val _subTitleFormat = resourceProvider.getString(R.string.paging_countable_sub_title_format)
 
-    private val _pagingManager = pagingManagerFactory.create(pagingItemRepository)
+    private val _pagingManager = pagingManagerFactory.create(_pagingItemRepository)
 
     init {
+        loadTitle()
         subscribePagedList()
      //   subscribeLoadErrorState()
     }
@@ -39,8 +44,8 @@ class CountablePagingViewModel @Inject constructor(
 
     private fun subscribePagedList() {
         val config = PagedList.Config.Builder()
-                .setInitialLoadSizeHint(20)
-                .setPageSize(10)
+                .setInitialLoadSizeHint(100)
+                .setPageSize(100)
                 .setPrefetchDistance(5)
                 .setEnablePlaceholders(false)
                 .build()
@@ -48,6 +53,15 @@ class CountablePagingViewModel @Inject constructor(
         RxPagedListBuilder<Int, PagingItemViewModel>(createDataSourceFactory(), config)
                 .buildObservable()
                 .doOnNext { listAdapter.submitList(it) }
+                .subscribe()
+                .addTo(_disposables)
+    }
+
+    private fun loadTitle() {
+        _pagingItemRepository.getTotalCount()
+                .subscribeOn(_schedulerFactory.io())
+                .observeOn(_schedulerFactory.ui())
+                .doOnSuccess { String.format(_subTitleFormat.toString(), it).run { subTitle.set(this) } }
                 .subscribe()
                 .addTo(_disposables)
     }
