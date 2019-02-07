74515a4c2f00a2e317c03fd033de0389dd266e21
Doohyun <skaengus2012@naver.com>
2019-02-07 11:50:16 +0900

    [ISSUE-11] BindingItemAdapter 전용 ListAdapter 제작

---
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/util/recyclerview/binding/BindingItemListAdapter.kt
@@ -0,0 +1,24 @@
+package nlab.practice.jetpack.util.recyclerview.binding
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import nlab.practice.jetpack.util.recyclerview.DiffCallback
+import nlab.practice.jetpack.util.recyclerview.DiffCallbackEx
+
+/**
+ * @author Doohyun
+ * @since 2019. 02. 07
+ */
+class BindingItemListAdapter<T: BindingItemViewModel>(
+        callback: DiffCallback<T>? = null): ListAdapter<T, BindingItemViewHolder>(callback?: DiffCallbackEx()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingItemViewHolder {
+        return BindingItemViewHolder.create(parent, viewType)
+    }
+
+    override fun onBindViewHolder(holder: BindingItemViewHolder, position: Int) {
+        getItem(position)?.run { holder.onBind(this) }
+    }
+
+    override fun getItemViewType(position: Int): Int = getItem(position).getLayoutRes()
+}
