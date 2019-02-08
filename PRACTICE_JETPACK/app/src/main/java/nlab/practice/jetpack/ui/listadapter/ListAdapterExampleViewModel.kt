c5305afa49c92ec742f93ce4900075ad8b978558
Doohyun <skaengus2012@naver.com>
2019-02-08 11:24:39 +0900

    [ISSUE-11] ListAdapter 예제에 에러페이지 및 swipe 기능 추가

--- PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/listadapter/ListAdapterExampleViewModel.kt
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/listadapter/ListAdapterExampleViewModel.kt
@@ -1,9 +1,11 @@
 package nlab.practice.jetpack.ui.listadapter
 
+import androidx.databinding.ObservableBoolean
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.subjects.BehaviorSubject
 import nlab.practice.jetpack.repository.PagingItemRepository
+import nlab.practice.jetpack.ui.common.viewmodel.ListErrorPageViewModel
 import nlab.practice.jetpack.util.SchedulerFactory
 import nlab.practice.jetpack.util.recyclerview.LayoutManagerFactory
 import nlab.practice.jetpack.util.recyclerview.RecyclerViewConfig
@@ -20,7 +22,7 @@ class ListAdapterExampleViewModel @Inject constructor(
         private val _disposables: CompositeDisposable,
         private val _schedulerFactory: SchedulerFactory,
         private val _pagingItemRepository: PagingItemRepository,
-        private val _listAdapterItemFactory: ListAdapterExampleItemViewModelFactory) {
+        private val _listAdapterItemFactory: ListAdapterExampleItemViewModelFactory) : ListErrorPageViewModel {
 
     companion object {
         const val SPAN_COUNT = 2
@@ -28,7 +30,11 @@ class ListAdapterExampleViewModel @Inject constructor(
 
     private val _listUpdateSubject: BehaviorSubject<List<ListAdapterExampleItemViewModel>> = BehaviorSubject.create()
 
+    private val _isShowErrorView = ObservableBoolean(false)
+
     val listAdapter = BindingItemListAdapter<ListAdapterExampleItemViewModel>()
+
+    val isShowRefreshProgressBar = ObservableBoolean(false)
 
     val recyclerViewConfig: RecyclerViewConfig
 
@@ -41,24 +47,38 @@ class ListAdapterExampleViewModel @Inject constructor(
         }
     }
 
+    override fun isShowErrorView(): ObservableBoolean = _isShowErrorView
+
+    override fun refresh() {
+        _pagingItemRepository.getItems(0, 200)
+                .subscribeOn(_schedulerFactory.io())
+                .doOnSuccess { list
+                    ->
+                    list.map { _listAdapterItemFactory.create(it) }.run { _listUpdateSubject.onNext(this) }
+                }
+                .observeOn(_schedulerFactory.ui())
+                .doOnSuccess { _isShowErrorView.set(false) }
+                .doOnError { _isShowErrorView.set(true) }
+                .doFinally {
+                    if (isShowRefreshProgressBar.get()) {
+                        isShowRefreshProgressBar.set(false)
+                    }
+                }
+                .subscribe()
+                .addTo(_disposables)
+    }
+
+    fun refreshBySwipeLayout() {
+        isShowRefreshProgressBar.set(true)
+        refresh()
+    }
+
     private fun initializeList() {
         _listUpdateSubject
                 .observeOn(_schedulerFactory.ui())
                 .subscribe { listAdapter.submitList(it) }
                 .addTo(_disposables)
 
-        loadList()
-    }
-
-    private fun loadList() {
-        _pagingItemRepository.getItems(0, 200)
-                .subscribeOn(_schedulerFactory.io())
-                .doOnSuccess {
-                    list
-                    ->
-                    list.map { _listAdapterItemFactory.create(it) }.run { _listUpdateSubject.onNext(this) }
-                }
-                .subscribe()
-                .addTo(_disposables)
+        refresh()
     }
 }
