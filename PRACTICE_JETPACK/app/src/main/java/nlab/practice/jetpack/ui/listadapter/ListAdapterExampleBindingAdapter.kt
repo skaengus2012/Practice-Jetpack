f215b0335c153681e594b27fe22acaa5aad99db9
Doohyun Nam <skaengus2012@naver.com>
2019-02-10 16:58:53 +0900

    [ISSUE-21] AppBar 쪽 모드 트랜지션 조금 더 자연스럽게 처리되도록 수정

--- PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/listadapter/ListAdapterExampleBindingAdapter.kt
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/ui/listadapter/ListAdapterExampleBindingAdapter.kt
@@ -4,18 +4,24 @@ import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import androidx.databinding.BindingAdapter
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import kotlinx.android.synthetic.main.fragment_list_adapter_example.view.*
+import nlab.practice.jetpack.R
+import nlab.practice.jetpack.util.AppBarStateChangeListener
+import nlab.practice.jetpack.util.ResourceProvider
 import nlab.practice.jetpack.util.SimpleAnimationListener
+
+
 
 /**
  * ListAdapterExample 에서 사용하는 BindingAdapter 열람
  *
  * @author Doohyun
  */
-@BindingAdapter("toolbarStateOfListAdapterExample")
-fun bindListAdapterExampleToolbarState(view: View, isSelectMode: Boolean) {
+@BindingAdapter("selectModeOfListAdapterExample")
+fun bindListAdapterExampleSelectMode(view: View, isSelectMode: Boolean) {
     val toolbarStandard = view.toolbarStandard
-    val toolbarSelect = view.toolbarSelect
 
     val currentStandardToolbarVisibility = toolbarStandard.visibility
     val targetStandardToolbarVisibility = if (isSelectMode) {
@@ -26,36 +32,90 @@ fun bindListAdapterExampleToolbarState(view: View, isSelectMode: Boolean) {
 
     val isDifferState = (currentStandardToolbarVisibility != targetStandardToolbarVisibility)
     if (isDifferState) {
-        val viewHeight = toolbarStandard.height.toFloat()
+        val toolbarSelect = view.toolbarSelect
+        val swipeRefreshLayout = view.swipeContainer
+        val collapsingToolbar = view.collapsingToolbar
+
         val isNeedShowStandardView = targetStandardToolbarVisibility == View.VISIBLE
 
         if (isNeedShowStandardView) {
-            toolbarSelect.visibility = View.GONE
+            swipeRefreshLayout.isEnabled = true
 
-            toolbarStandard.animate().setDuration(200)
-                    .withStartAction {
-                        toolbarStandard.visibility = View.VISIBLE
-                    }
-                    .translationY(0f)
-                    .withLayer()
+            setCollapsingEnterAlwaysMode(collapsingToolbar)
+            convertStandardToolbarTransition(toolbarStandard, toolbarSelect)
 
         } else {
-            toolbarStandard.animate().setDuration(100)
-                    .withEndAction {
-                        toolbarStandard.visibility =  View.GONE
+            val appbarLayout = view.appbarLayout
 
-                        AlphaAnimation(0f, 1f).apply {
-                            duration = 300
+            // 선택모드에선 리프레쉬 안되게 고정
+            swipeRefreshLayout.isEnabled = false
 
-                            setAnimationListener(object: SimpleAnimationListener() {
-                                override fun onAnimationStart(animation: Animation?) {
-                                    toolbarSelect.visibility = View.VISIBLE
-                                }
-                            })
-                        }.run {  toolbarSelect.startAnimation(this) }
+            // Toolbar 가 완전히 내려온 상태가 아니라면, 내린다음 트랜지션을 수행
+            // 리스너는 등록하고 바로 해제하는 용도로 사용
+            val instantAppBarListener = object: AppBarStateChangeListener() {
+                override fun onStateChanged(appBarLayout: AppBarLayout?, state: Int) {
+                    if (state == State.EXPANDED) {
+                        setCollapsingDisableMode(collapsingToolbar)
+                        convertSelectToolbarTransition(toolbarStandard, toolbarSelect)
+
+                        appbarLayout.removeOnOffsetChangedListener(this)
                     }
-                    .translationY(-viewHeight)
-                    .withLayer()
+                }
+            }
+
+            appbarLayout.addOnOffsetChangedListener(instantAppBarListener)
+            appbarLayout.setExpanded(true, true)
         }
     }
+}
+
+private fun setCollapsingEnterAlwaysMode(collapsingLayout: CollapsingToolbarLayout) {
+    getAppBarLayoutParams(collapsingLayout)?.run {
+        scrollFlags = AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
+        collapsingLayout.layoutParams = this
+    }
+}
+
+private fun getAppBarLayoutParams(collapsingLayout: CollapsingToolbarLayout): AppBarLayout.LayoutParams? {
+    return collapsingLayout.layoutParams as? AppBarLayout.LayoutParams
+}
+
+private fun convertStandardToolbarTransition(toolbarStandard: View, toolbarSelect: View) {
+    toolbarSelect.visibility = View.GONE
+
+    toolbarStandard.animate().setDuration(200)
+            .withStartAction { toolbarStandard.visibility = View.VISIBLE }
+            .translationY(0f)
+            .withLayer()
+}
+
+private fun setCollapsingDisableMode(collapsingLayout: CollapsingToolbarLayout) {
+    collapsingLayout.minimumHeight =
+            ResourceProvider(collapsingLayout.context).getDimensionPixelSize(R.dimen.list_adapter_toolbar_height)
+
+    getAppBarLayoutParams(collapsingLayout)?.run {
+        scrollFlags = 0
+        collapsingLayout.layoutParams = this
+    }
+}
+
+private fun convertSelectToolbarTransition(toolbarStandard: View, toolbarSelect: View) {
+    val viewHeight = toolbarStandard.height.toFloat()
+
+    toolbarStandard.animate().setDuration(100)
+            .withEndAction {
+                toolbarStandard.visibility =  View.GONE
+
+                AlphaAnimation(0f, 1f).apply {
+                    duration = 300
+
+                    setAnimationListener(object: SimpleAnimationListener() {
+                        override fun onAnimationStart(animation: Animation?) {
+                            toolbarSelect.visibility = View.VISIBLE
+                        }
+                    })
+                }.run {  toolbarSelect.startAnimation(this) }
+            }
+            .translationY(-viewHeight)
+            .withLayer()
 }
