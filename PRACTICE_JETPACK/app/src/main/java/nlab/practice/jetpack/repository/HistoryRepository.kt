9777bddf804f1305907a7c0da168332cb26e7b81
Doohyun Nam <skaengus2012@naver.com>
2019-02-11 10:32:59 +0900

    [N/A] ListAdapter, Selection 에 대한 History 추가

--- PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/repository/HistoryRepository.kt
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/repository/HistoryRepository.kt
@@ -20,7 +20,13 @@ class HistoryRepository {
                     "아이디어는 좋았으나, Bottom Nav 에 적용하는 기존 메소드들의 커스텀이 힘듬(애니메이션, 규칙 등)\n프래그먼트가 무조건 파괴되고 재생성\n메모리 릭이 있음"),
 
             History("JetPack Paging Module", true,
-                    "Countable, Unbound 상황에 대한 페이징 로직 제작\n실패, 새로고침에 대한 로직을 smooth 하게 되도록 구성해봄")
+                    "Countable, Unbound 상황에 대한 페이징 로직 제작\n실패, 새로고침에 대한 로직을 smooth 하게 되도록 구성해봄"),
+
+            History("JetPack ListAdapter", true,
+                    "submitList 만 적절하게 처리하면, 간단하고 쉽게 Adapter 의 트랜잭션을 처리할 수 있음.\n애니메이션도 좋아서 매우 쓸만함"),
+
+            History("JetPack RecyclerView Selection", true,
+                    "기본적인 Selection 처리에 대해 다루어 봤음\n이를 적용하기에 몇가지 제약이 있을 수 있음.\n  * 같은 키 사용 못함\n  * Select 된 항목이 하나도 없을 경우 Selection 자동해지\n  * 너무 추가해야할 코드가 많을 수 있음\n    (Selection 항목에 대한 패턴화가 필요할 것 같음)")
     )
 
 }
