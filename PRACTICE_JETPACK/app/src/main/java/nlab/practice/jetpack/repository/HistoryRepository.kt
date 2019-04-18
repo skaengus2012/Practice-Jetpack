cbc0e2827e8c8db05298d70c7a1190297553678a
Doohyun Nam <skaengus2012@naver.com>
2019-04-19 01:00:12 +0900

    [N/A] CollapsingToolbarLayout 에 대한 History 추가

--- PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/repository/HistoryRepository.kt
+++ PRACTICE_JETPACK/app/src/main/java/nlab/practice/jetpack/repository/HistoryRepository.kt
@@ -11,10 +11,8 @@ class HistoryRepository {
             History("Anko", false,
                     "기술의 사용은 가능하나, MVVM 사용 불가.\nPreview 가 제대로 동작안해 불편\nAndroid X 혼용 불명.."),
 
-
             History("MVVM with Dagger", true,
                     "AAC 사용안하는 MVVM 사용으로, 모든 로직을 ViewModel 과 xml 에서 처리 가능\nTDD 가능\n대신 Usecase 종류가 많아짐\nView 속성의 경계가 점점 애매모호.. ㅡㅡ^"),
-
 
             History("JetPack Navigation", false,
                     "아이디어는 좋았으나, Bottom Nav 에 적용하는 기존 메소드들의 커스텀이 힘듬(애니메이션, 규칙 등)\n프래그먼트가 무조건 파괴되고 재생성\n메모리 릭이 있음"),
@@ -29,7 +27,10 @@ class HistoryRepository {
                     "기본적인 Selection 처리에 대해 다루어 봤음\n이를 적용하기에 몇가지 제약이 있을 수 있음.\n  * 같은 키 사용 못함\n  * Select 된 항목이 하나도 없을 경우 Selection 자동해지\n  * 너무 추가해야할 코드가 많을 수 있음\n    (Selection 항목에 대한 패턴화가 필요할 것 같음)"),
 
             History("RecyclerView ItemTouchHelper", true,
-                    "Drag & Drop 과 Swipe 는 ItemTouchHelper 의 구현만으로 쉽게 할 수 있음.\n  * DI 로 연결해야하는 이슈가 있었음\n  *  Multi-binding <-> Named Tag 주목")
+                    "Drag & Drop 과 Swipe 는 ItemTouchHelper 의 구현만으로 쉽게 할 수 있음.\n  * DI 로 연결해야하는 이슈가 있었음\n  *  Multi-binding <-> Named Tag 주목"),
+
+            History("CollapsingToolbar Layout 연습", true,
+                    "CoordinatorLayout, AppBarLayout 과 함께, Collapsing 기능을 좀 더 자세히 구현할 수 있음\n  * 기본적인 예제 구현사항임\n  *  CoordinatorLayout anchor 연구는 해봐야함")
     )
 
 }
